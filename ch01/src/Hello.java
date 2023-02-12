b347229e3f37b5e07d1d0d7c6713ed962338b90c
ykmr0331 <ykmr0331@naver.com>
2023-02-12 15:13:57 +0900

    230212_1

--- ch01/src/Hello.java
+++ ch01/src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world."); //화면에 글자를 출력한다.
+		System.out.println("Hell, world."); //화면에 글자를 출력한다.
+		
 	}
 
 }
---
+++ ch01/src/j230212_01.java
@@ -0,0 +1,30 @@
+
+public class j230212_01 { //ch1-13~16
+
+	public static void main(String[] args) {
+		//ctrl +shft+ l: 단축키 전체목록 보기
+		//ctrl + '+','-': 폰트크기 증가,감소
+		//ctrl + d  :한줄 삭제
+		//ctrl + alt +  + down : 행단위 복사
+		//alt + shift + A : 멀티컬럼 편집
+		//alt + up, down: 행단위 이동
+		//tab: 들여쓰기
+		//shift + tab: 내어쓰기
+		//ctrl + I : 자동으로 들여쓰기 잡아주는거
+		//한행 또는 여려행 선택한다음 ctrl + /: 주석처리(토글)
+		//여러줄 주석처리: /* 여러줄 */
+		//ctrl + space :자동완성
+		System.out.println("Hell, world."); 
+		System.out.println("Hell, world."); 
+		System.out.println("Hell, world."); 
+		System.out.println("Hell, world."); 
+		System.out.println("Hell, world."); 
+		System.out.println("Hell, world.");
+		/*sysout 입력하고 ctrl + space 누르면 위에 양식이 
+		자동 입력된다. 
+		양식을 바꿀려면    window → preference → java
+		→ editor → templates 여기서 찾을 수 있다.
+		바꾸고 마지막에 no눌러야된다.*/
+	
+	}
+}
