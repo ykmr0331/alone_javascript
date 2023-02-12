5f3c176e4b386b14cb1e43b1ba30d029e626c7be
ykmr0331 <ykmr0331@naver.com>
2023-02-12 16:22:09 +0900

    230212

---
+++ _ch02/src/j230212_04.java
@@ -0,0 +1,18 @@
+
+public class j230212_04 {
+//ch2-3 변수란? 변수의 선언과 저장
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		int age = 14, year = 0;  //int를 입력하면  하나의 문장 안에 있는 age와 year 둘다 상수로 변하는듯?
+		int clot = 3000, floo = 5000;
+		year =age + 2000 + clot + floo;
+		age = 14 + 1;
+		System.out.println(year);
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ ch02_new/src/J230212_05_VarEx1.java
@@ -0,0 +1,16 @@
+
+public class J230212_05_VarEx1 {
+//ch2-3 변수란? 변수의 선언과 저장
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("6 + 3");//큰따옴표 안에 있는 내용들은 그대로 출력되기 때문에
+		System.out.println(6 + 3); //이러면 9나온다
+		int x = 4, y = 2;
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+	}
+
+}
+
